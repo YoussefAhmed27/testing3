f661f485138216247c3101ce68b93e676d2348ce
Youssef <youssefahmed77x@gmail.com>
2026-05-15 23:37:13 +0300

    v7

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,8 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://practicesoftwaretesting.com',
@@ -21,7 +26,7 @@ module.exports = defineConfig({
           plugins: [createEsbuildPlugin.default(config)],
         })
       );
-      
+
       return config;
     },
   },
