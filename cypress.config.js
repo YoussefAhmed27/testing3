95a12cc1bdbfaddc9ff9d55145dc55b98ff14bde
Youssef <youssefahmed77x@gmail.com>
2026-05-15 23:48:16 +0300

    v8

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,10 @@ const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esb
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://practicesoftwaretesting.com',
+    
+
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    
     specPattern: "**/*.feature",
     chromeWebSecurity: false,
     watchForFileChanges: false,
